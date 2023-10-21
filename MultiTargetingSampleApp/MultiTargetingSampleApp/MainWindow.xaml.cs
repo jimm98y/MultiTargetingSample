fa06db1674e33e2b12703022415ce7474cd3dca0
Lukas Volf <jimm98y@hotmail.com>
2023-10-21 02:30:53 +0200

    ***initialize repo***

---
+++ MultiTargetingSampleApp/MultiTargetingSampleApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace MultiTargetingSampleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            textBlock1.Text = SampleLibrary.SampleClass.GetSampleString() +
+                $" running inside {System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture} host.";
+        }
+    }
+}
---
+++ MultiTargetingSampleApp/SampleLibrary/SampleClass.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleLibrary
+{
+    public static class SampleClass
+    {
+        public static string GetSampleString()
+        {
+            return "Hello world from the AnyCPU netstandard2.0";
+        }
+    }
+}
